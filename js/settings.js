6c0a273227eec0205c27c4c01238cff69954c3ef
Nazım Sucu <99741791+nazms23@users.noreply.github.com>
2024-06-22 16:18:57 +0300

    Settings 1
    Html content has been created.
    Display of uploaded gifs is done

---
+++ js/settings.js
@@ -0,0 +1,20 @@
+const { ipcRenderer } = require("electron");
+
+
+ipcRenderer.on('gifs',(err,data)=>{
+
+    const slc = document.querySelector("#gifselect");
+    slc.size = data.gifs.length;
+    data.gifs.forEach(d => {
+        let opt = document.createElement("option");
+        if(data.act == d)
+        {
+            opt.selected = true;
+        }
+        opt.value = d;
+        opt.innerHTML = d;
+        slc.appendChild(opt);
+        console.log(d)
+    });
+
+})
--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const {app,BrowserWindow, Tray, Menu} = require('electron');
+const fs = require('fs');
 
 app.on('ready', async () => {
     const win = new BrowserWindow({
@@ -18,9 +19,33 @@ app.on('ready', async () => {
 
     let tray = new Tray('./resim/pngegg.png')
     const contextMenu = Menu.buildFromTemplate([
-        { label: 'Close', type: 'normal', role:"quit"},
-        { label: 'Ayarlar', type: 'normal' },
+        { label: 'Settings', type: 'normal', click: () => {
+            const settwin = new BrowserWindow({
+                width: 500,
+                height: 500,
+                title: "Settings",
+                webPreferences: {
+                    nodeIntegration:true,
+                    contextIsolation: false
+                }
+    
+            })
+            settwin.loadFile("./html/settings.html")
+            
+            settwin.on('ready-to-show',async () => {
+                fs.readdir("./gif/", (err,files) => {
+                    files.forEach((fn) => {
+                        console.log(fn)
+                    })
+                    
+                    settwin.webContents.send('gifs', {gifs:files, act: "konata-dance.gif"});})
+            })
+
+        }},
+        { label: 'Close', type: 'normal', role:"quit"}
       ])
       tray.setToolTip('Desktop Gif App')
       tray.setContextMenu(contextMenu)
+
+    
 })
